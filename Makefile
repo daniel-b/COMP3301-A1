e475e0e5b5a67904ff867a7f574911fcb44a6136
Daniel <thetinman21@gmail.com>
2013-07-24 21:05:35 +1000

    getcommand() added
    changes to makefile

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
+all: clean qshell #small project so clean first
+
 qshell: qshell.c
-	gcc -g -Wall -pedantic -std=c99 -o $@ $^
+	gcc -g -Wall -pedantic -ansi -o $@ $^ #remove -g for sumbission
+	
+clean: #doesn't really do anything as -o option is ussed currently
+	rm -rf *.o qshell
+	 
--- qshell.c
+++ qshell.c
@@ -4,11 +4,13 @@
 
 /* Function Prototypes */
 int printprompt(void);
+int getcommand(void);
 
 int main(void)
 {
-	//printf("Test");
+	/*printf("Test");*/
 	printprompt();
+	getcommand();
 	return 0;
 }
 
@@ -16,9 +18,31 @@ int printprompt(void)
 {
 	char cwd[1024];
 	if(getcwd(cwd, sizeof(cwd)) != NULL)
+	{
 		printf("%s?", cwd);
 		return 0;
+	}
 	else
 		printf("getcwd() error?");
 	return -1;
+}
+
+/*char*[] getcommand(void)*/
+int getcommand(void)
+{
+	int bytes_read;
+	int no_bytes = 128;/*maximum number of characters that will be read(can type more)*/
+	char *command;
+	
+	/*allocate memory for string. + 1 for null char*/
+	command = (char *) malloc (no_bytes + 1);
+	bytes_read = getline(&command, &no_bytes, stdin);
+	
+	if(bytes_read > 128)
+		printf("bytes_read > 128- No Read: %d\n", bytes_read);
+	else if(bytes_read < 1)
+		printf("bytes_read < 1- No Read: %d\n", bytes_read);
+	else
+		printf("bytes_read: %d\n", bytes_read);
+	return 0;
 }
